854c9e6ccf84306191f516cdc3de0cb8391db6db
Zheng Jin <mkim0407@gmail.com>
2023-04-02 18:38:39 -0700

    connect source game data

--- prepare-data/src/main.py
+++ prepare-data/src/main.py
@@ -1,2 +1,20 @@
+import os
+
+
+def prepare_data(source, output, version_name):
+    print(source, output, version_name)
+    print(os.listdir(source))
+
+
+def main(args=None):
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--source', default='/source')
+    parser.add_argument('--output', default='/data')
+    parser.add_argument('--version-name', default='Latest')
+    args = parser.parse_args(args)
+    prepare_data(**vars(args))
+
+
 if __name__ == '__main__':
-    print('hello world')
+    main()
